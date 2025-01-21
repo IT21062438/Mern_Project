806f86f3c25d94b6a78ded223145ff13b5e962e0
Harendra Samarawickrama <87431873+IT21062438@users.noreply.github.com>
2025-01-22 00:46:32 +0530

    Search created

--- frontend/src/Components/UserDetails/UserDetails.js
+++ frontend/src/Components/UserDetails/UserDetails.js
@@ -7,41 +7,72 @@ import { useReactToPrint } from "react-to-print";
 const URL = "http://localhost:5000/users";
 
 function UserDetails() {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
+  const [searchQuery, setSearchQuary] = useState("");
+  const [noResults, setNoResults] = useState(false);
+
+  const fetchHandler = async () => {
+    try {
+      const response = await axios.get(URL);
+      return response.data.users;
+    } catch (err) {
+      setError("Failed to fetch user data. Please try again.");
+      return [];
+    }
+  };
 
   useEffect(() => {
-    const fetchHandler = async () => {
-      try {
-        const response = await axios.get(URL);
-        setUsers(response.data.users);
-      } catch (err) {
-        setError("Failed to fetch user data. Please try again.");
-      }
-    };
-    fetchHandler();
+    fetchHandler().then((data) => {
+      setUsers(data);
+    });
   }, []);
+
+  const handleSearch = () => {
+    fetchHandler().then((data) => {
+      const filteredUsers = data.filter((user) =>
+        Object.values(user).some((field) =>
+          field.toString().toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+      setUsers(filteredUsers);
+      setNoResults(filteredUsers.length === 0);
+    });
+  };
 
   const ComponentsRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => ComponentsRef.current,
     documentTitle: "Users Report",
-    onAfterPrint: () => alert("User Report Successfully Download !"),
+    onAfterPrint: () => alert("User Report Successfully Downloaded!"),
   });
 
   return (
     <div>
       <Nav />
       <h2>User Details Display Page</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <div ref={ComponentsRef}>
-        {users &&
-          users.map((user, i) => (
-            <div key={i}>
-              <User user={user} />
-            </div>
-          ))}
-      </div>
+      <input
+        onChange={(e) => setSearchQuary(e.target.value)}
+        type="text"
+        name="search"
+        placeholder="Search User Details"
+      />
+      <button onClick={handleSearch}>Search</button>
+      {noResults ? (
+        <div>
+          <p>No Users Found</p>
+        </div>
+      ) : (
+        <div ref={ComponentsRef}>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          {users &&
+            users.map((user, i) => (
+              <div key={i}>
+                <User user={user} />
+              </div>
+            ))}
+        </div>
+      )}
       <button onClick={handlePrint}>Download Report</button>
     </div>
   );
