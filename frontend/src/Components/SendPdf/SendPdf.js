d090289e314cdaf949ad9f04b48559d4f924f21e
Harendra Samarawickrama <87431873+IT21062438@users.noreply.github.com>
2025-01-26 19:02:51 +0530

    Update SendPdf.js

--- frontend/src/Components/SendPdf/SendPdf.js
+++ frontend/src/Components/SendPdf/SendPdf.js
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "../Nav/Nav";
 import axios from "axios";
 import PdfView from "./PdfView";
